600c0cab8e24983c99153e9164729c2c09d74a71
unknown <sirilo99x3@gmail.com>
2025-11-02 22:10:03 -0600

    Ver Mascota HTML

--- src/app/components/ver-mascota/ver-mascota.ts
+++ src/app/components/ver-mascota/ver-mascota.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import { RouterLink } from "@angular/router";
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-ver-mascota',
-  imports: [],
+  imports: [MatCardModule, RouterLink, MatButtonModule],
   templateUrl: './ver-mascota.html',
   styleUrl: './ver-mascota.css',
 })
