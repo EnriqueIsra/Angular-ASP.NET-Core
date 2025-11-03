c153dca71075e8353e657a5980b5d5fa955b869c
unknown <sirilo99x3@gmail.com>
2025-11-03 01:22:08 -0600

    Agregar Mascota HTML

--- src/app/components/agregar-editar-mascota/agregar-editar-mascota.ts
+++ src/app/components/agregar-editar-mascota/agregar-editar-mascota.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { Spinner } from '../../shared/spinner/spinner';
+import { MatCard, MatCardSubtitle } from "@angular/material/card";
+import {MatGridListModule} from '@angular/material/grid-list';
+import { MatFormField, MatLabel } from "@angular/material/form-field";
+import { MatInput } from "@angular/material/input";
+import {MatButtonModule} from '@angular/material/button';
+import { RouterLink } from "@angular/router";
+
 
 @Component({
   selector: 'app-agregar-editar-mascota',
-  imports: [],
+  imports: [Spinner, 
+    MatCard, 
+    MatCardSubtitle, 
+    MatGridListModule, 
+    MatFormField, MatLabel, 
+    MatInput, 
+    MatButtonModule, 
+    RouterLink],
   templateUrl: './agregar-editar-mascota.html',
   styleUrl: './agregar-editar-mascota.css',
 })
 export class AgregarEditarMascota {
-
+  loading: boolean = false;
 }
